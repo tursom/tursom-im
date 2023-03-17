eb75e8605c0397ff4265225cea45251013e1c37b
tursom <tursom@foxmail.com>
2023-03-17 14:01:14 +0800

    创建分布式通讯支持

--- distributed/Cluster.go
+++ distributed/Cluster.go
@@ -3,9 +3,11 @@ package distributed
 import (
 	"context"
 	"fmt"
+	"math"
 	"sync"
 	"time"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/tursom/GoCollections/exceptions"
 	"github.com/tursom/GoCollections/lang"
 	"github.com/tursom/GoCollections/lang/atomic"
@@ -13,59 +15,86 @@ import (
 	"github.com/tursom-im/exception"
 )
 
+const (
+	UNREACHABLE = math.MaxUint32
+)
+
 type (
+	// MessageProcessor 用于处理底层消息传输的接口
+	// 由用户负责实现
 	MessageProcessor[Msg any] interface {
+		// LocalId 本地节点 ID
 		LocalId() string
 
+		// Send 对目标节点发送消息转发请求
+		// return false 表示节点不可达
 		Send(
 			ctx context.Context, nextJmp string, target string, msg Msg, jmp uint32,
 		) (bool, exceptions.Exception)
 
+		// Find 对 nextJmp 节点发送路由查询请求
+		//
+		// return 从本节点出发，经 nextJmp 到 target 的拓扑距离。
+		//        UNREACHABLE 表示节点不可达
 		Find(
 			ctx context.Context, nextJmp string, target string, jmp uint32,
-		) (int32, exceptions.Exception)
+		) (uint32, exceptions.Exception)
 	}
 
+	// Cluster 负责集群管理、消息转发等
 	Cluster[Msg any] interface {
+		// Send 向指定 id 的节点发送消息
+		// jmp 消息已经经过了几个节点的转发
 		Send(
 			ctx context.Context, id string, msg Msg, jmp uint32,
 		) (bool, exceptions.Exception)
 
+		// Find 查询到目标节点的拓扑距离
+		// return UNREACHABLE 表示节点不可达
 		Find(
-			ctx context.Context, id string, distance int32, jmp uint32,
-		) (int32, exceptions.Exception)
+			ctx context.Context, id string, jmp uint32,
+		) (uint32, exceptions.Exception)
 
+		// AddNodes 添加集群节点记录
 		AddNodes(nodes []string)
-		ConnectedNodes(nodes []string)
+
+		// SetConnected 设置已连接的节点列表
+		SetConnected(nodes []string)
 	}
 
 	clusterImpl[Msg any] struct {
 		processor MessageProcessor[Msg]
 		maxJmp    uint32
-		version   uint32
 		connected map[string]*node
 		nodes     map[string]*node
 		lock      sync.Mutex
-		topology  atomic.Reference[topology]
+		cluster   atomic.Reference[clusterNodes]
 	}
 
-	topology struct {
+	clusterNodes struct {
 		version uint32
 		nodes   map[string]byte
 	}
 
 	node struct {
-		id              string
-		snapshotVersion uint32
-		nextJmp         *node
-		distance        uint32
-		lock            sync.Mutex
+		id             string
+		clusterVersion uint32
+		nextJmp        nextJmpReference
+		lock           sync.Mutex
+	}
+
+	nextJmpReference struct {
+		atomic.Reference[nextJmpNode]
 	}
 
 	nextJmpNode struct {
 		node     *node
 		distance uint32
 	}
+)
+
+var (
+	unreachable = &nextJmpNode{distance: UNREACHABLE}
 )
 
 func NewCluster[Msg any](config *ClusterConfig[Msg]) Cluster[Msg] {
@@ -75,7 +104,7 @@ func NewCluster[Msg any](config *ClusterConfig[Msg]) Cluster[Msg] {
 		connected: make(map[string]*node),
 		nodes:     make(map[string]*node),
 	}
-	c.topology.Store(&topology{})
+	c.cluster.Store(&clusterNodes{})
 	return c
 }
 
@@ -86,71 +115,68 @@ func (c *clusterImpl[Msg]) Send(
 		return false, nil
 	}
 
-	node := c.getNode(id)
+	node := c.nodeOf(id)
 
-	topology := c.topology.Load()
-	if node.snapshotVersion != topology.version {
+	topology := c.cluster.Load()
+	if node.clusterVersion != topology.version {
 		if _, ok := topology.nodes[id]; !ok {
 			return false, exception.NewNodeOfflineException(fmt.Sprintf("node %s is offline", id))
 		}
 	}
 
-	return c.sendToNode(ctx, node, msg, jmp+1)
-}
-
-func (c *clusterImpl[Msg]) Find(ctx context.Context, id string, distance int32, jmp uint32) (int32, exceptions.Exception) {
-	if jmp > c.maxJmp {
-		return -1, nil
+	ok, e := c.sendToNode(ctx, node, msg, jmp+1)
+	if !ok && e != nil {
+		c.removeNode(id)
 	}
 
+	return ok, e
+}
+
+func (c *clusterImpl[Msg]) Find(ctx context.Context, id string, jmp uint32) (uint32, exceptions.Exception) {
 	if c.processor.LocalId() == id {
 		return 0, nil
 	}
 
-	_, ok := c.topology.Load().nodes[id]
-	if !ok {
-		return -1, nil
-	}
-	n := c.getNode(id)
-	if n.distance > 0 {
-		return int32(n.distance) + distance, nil
+	if jmp > c.maxJmp {
+		return UNREACHABLE, nil
 	}
 
-	if e := c.discovery(ctx, n, jmp); e != nil {
-		return -1, e
+	if _, ok := c.cluster.Load().nodes[id]; !ok {
+		return UNREACHABLE, nil
 	}
 
-	return int32(n.distance), nil
+	n := c.nodeOf(id)
+	if !n.nextJmp.unreachable() {
+		return n.nextJmp.distance() + jmp, nil
+	}
+
+	c.discovery(ctx, n, jmp)
+
+	return n.nextJmp.distance(), nil
 }
 
 func (c *clusterImpl[Msg]) AddNodes(nodes []string) {
-	topologySnapshot := c.topology.Load()
+	topologySnapshot := c.cluster.Load()
 
 	m, changed := sum(topologySnapshot.nodes, nodes)
-	for changed && !c.topology.CompareAndSwap(topologySnapshot, &topology{
+	for changed && !c.cluster.CompareAndSwap(topologySnapshot, &clusterNodes{
 		version: topologySnapshot.version + 1,
 		nodes:   m,
 	}) {
 		m, changed = sum(topologySnapshot.nodes, nodes)
-		topologySnapshot = c.topology.Load()
+		topologySnapshot = c.cluster.Load()
 	}
 }
 
-func (c *clusterImpl[Msg]) ConnectedNodes(nodes []string) {
+func (c *clusterImpl[Msg]) SetConnected(nodes []string) {
 	c.AddNodes(nodes)
 
-	c.lock.Lock()
-	defer c.lock.Unlock()
-
-	c.connected = make(map[string]*node)
+	connected := make(map[string]*node)
 	for _, id := range nodes {
-		n := c.nodes[id]
-		if n == nil {
-			n = &node{id: id}
-			c.nodes[id] = n
-		}
-		c.connected[id] = n
+		connected[id] = c.nodeOf(id)
 	}
+
+	c.connected = connected
 }
 
 func sum(m map[string]byte, s []string) (map[string]byte, bool) {
@@ -171,8 +197,9 @@ func sum(m map[string]byte, s []string) (map[string]byte, bool) {
 	return newMap, chanced
 }
 
+// removeNode 从集群中删除节点
 func (c *clusterImpl[Msg]) removeNode(node string) {
-	topologySnapshot := c.topology.Load()
+	topologySnapshot := c.cluster.Load()
 	if _, ok := topologySnapshot.nodes[node]; !ok {
 		return
 	}
@@ -183,11 +210,11 @@ func (c *clusterImpl[Msg]) removeNode(node string) {
 	}
 	delete(m, node)
 
-	for !c.topology.CompareAndSwap(topologySnapshot, &topology{
+	for !c.cluster.CompareAndSwap(topologySnapshot, &clusterNodes{
 		version: topologySnapshot.version + 1,
 		nodes:   m,
 	}) {
-		topologySnapshot = c.topology.Load()
+		topologySnapshot = c.cluster.Load()
 		if _, ok := topologySnapshot.nodes[node]; !ok {
 			return
 		}
@@ -198,15 +225,19 @@ func (c *clusterImpl[Msg]) removeNode(node string) {
 		}
 		delete(m, node)
 	}
+
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	delete(c.nodes, node)
+	delete(c.connected, node)
 }
 
 func (c *clusterImpl[Msg]) sendToNode(ctx context.Context, node *node, msg Msg, jmp uint32) (bool, exceptions.Exception) {
-	if node.nextJmp == nil {
-		if e := c.discovery(ctx, node, jmp); e != nil {
-			return false, e
-		}
+	if node.nextJmp.unreachable() {
+		c.discovery(ctx, node, jmp)
 
-		if node.nextJmp == nil {
+		if node.nextJmp.unreachable() {
 			return false, exception.NewNodeOfflineException(fmt.Sprintf("node %s is offline", node.id))
 		}
 	}
@@ -215,115 +246,127 @@ func (c *clusterImpl[Msg]) sendToNode(ctx context.Context, node *node, msg Msg, 
 }
 
 func (c *clusterImpl[Msg]) send(ctx context.Context, node *node, msg Msg, jmp uint32) (bool, exceptions.Exception) {
-	ok, e := c.processor.Send(ctx, node.nextJmp.id, node.id, msg, jmp)
-	if ok || e != nil {
+	if ok, e := c.processor.Send(ctx, node.nextJmp.nodeId(), node.id, msg, jmp); ok || e != nil {
 		return ok, e
 	}
 
-	if _, ok := c.nodes[node.id]; !ok {
-		return false, nil
+	// 发送失败，尝试重新查找节点并发送消息
+	// 最多重试 3 次
+	for retryTimes := 0; retryTimes < 3; retryTimes++ {
+		log.Warnf("failed to send msg to %s, retry %d times", node.id, retryTimes+1)
+
+		if _, ok := c.nodes[node.id]; !ok {
+			return false, nil
+		}
+
+		node.nextJmp.Store(unreachable)
+
+		c.discovery(ctx, node, jmp)
+
+		// 不可达，重新尝试
+		if node.nextJmp.unreachable() {
+			continue
+		}
+
+		// 如果发送失败则重试
+		if send, e := c.processor.Send(ctx, node.nextJmp.nodeId(), node.id, msg, jmp); send || e != nil {
+			return send, e
+		}
 	}
 
-	node.nextJmp = nil
-	if e := c.discovery(ctx, node, jmp); e != nil {
-		return false, e
-	}
-
-	if node.nextJmp == nil {
-		return false, nil
-	}
-
-	return c.processor.Send(ctx, node.nextJmp.id, node.id, msg, jmp)
+	return false, nil
 }
 
-func (c *clusterImpl[Msg]) discovery(ctx context.Context, n *node, jmp uint32) exceptions.Exception {
+// discovery 尝试发现目标节点
+// jmp 查询请求已经经过的节点数量
+func (c *clusterImpl[Msg]) discovery(ctx context.Context, n *node, jmp uint32) {
+	// 本机节点直接返回
+	if n.id == c.processor.LocalId() {
+		n.nextJmp.Store(&nextJmpNode{node: n, distance: 0})
+		return
+	}
+
+	// 上锁，防止同时进行冗余查询，浪费系统资源
 	n.lock.Lock()
 	defer n.lock.Unlock()
-	if n.nextJmp != nil {
-		return nil
+
+	// double check，确认是否已经找到目标节点
+	if !n.nextJmp.unreachable() {
+		return
 	}
 
-	if n.id == c.processor.LocalId() {
-		n.nextJmp = n
-		return nil
-	}
-
-	_, ok := ctx.Deadline()
-	if !ok {
+	if _, ok := ctx.Deadline(); !ok {
+		//goland:noinspection GoVetLostCancel
 		ctx, _ = context.WithTimeout(ctx, 45*time.Second)
 	}
 
+	// connected nodes snapshot
 	connected := c.connected
 
 	finishedCount := new(atomic.Int32)
-	var done lang.Channel[bool] = make(lang.RawChannel[bool])
+	done := make(lang.RawChannel[bool])
 
-	var nextJmp *nextJmpNode
-
+	// 遍历已连接节点，发送寻人启事
 	for _, next := range connected {
-		go c.check(ctx, next, n, jmp, func(node string, distance int32, e exceptions.Exception) {
-			if distance <= 0 {
+		njn := &nextJmpNode{
+			node:     next,
+			distance: 1,
+		}
+
+		// 发现下一跳正好是目标节点，立即存储
+		// 但是由于分布式环境充满不确定性，还是要发送确认消息的
+		if next.id == n.id {
+			n.nextJmp.Store(njn)
+		}
+
+		// 启动新 goroutine 发送请求
+		go c.check(ctx, next, n, jmp, func(node string, distance uint32, e exceptions.Exception) {
+			if distance == UNREACHABLE || e != nil {
+				n.nextJmp.CompareAndSwap(njn, unreachable)
+
+				// 确保在所有节点都返回不可达时，能够正确返回
 				if finishedCount.Add(1) == int32(len(connected)) {
 					done.TrySend(true)
 				}
-				return
-			}
 
-			nextJmp = &nextJmpNode{
-				node:     next,
-				distance: uint32(distance),
-			}
-
-			go func() {
-				n.lock.Lock()
-				defer n.lock.Unlock()
-
-				if n.distance > uint32(distance) {
-					n.nextJmp = next
-					n.distance = uint32(distance)
+				if e != nil {
+					log.Warnf("failed to check for node %s: %s", next.id, e)
+					e.PrintStackTrace()
 				}
-			}()
+			}
+
+			n.nextJmp.update(&nextJmpNode{
+				node:     next,
+				distance: distance,
+			})
 
 			done.TrySend(true)
 		})
 	}
 
-	_, ok = done.ReceiveTimeout(time.Minute)
-	if !ok {
-		return exception.NewTimeoutException("")
+	if _, ok := done.ReceiveTimeout(time.Minute); !ok {
+		log.Warnf("discovery to %s timeout", n.id)
 	}
-
-	nextJmp0 := nextJmp
-	if nextJmp0 != nil {
-		n.nextJmp = nextJmp0.node
-		n.distance = nextJmp0.distance
-	}
-	return nil
-
 }
 
-func (c *clusterImpl[Msg]) check(ctx context.Context, nextJmp, n *node, jmp uint32, done func(node string, distance int32, e exceptions.Exception)) {
+func (c *clusterImpl[Msg]) check(
+	ctx context.Context,
+	nextJmp, n *node,
+	jmp uint32,
+	done func(node string, distance uint32, e exceptions.Exception),
+) {
 	distance, e := c.processor.Find(ctx, nextJmp.id, n.id, jmp)
 	if e != nil {
-		done(n.id, -1, e)
+		done(n.id, UNREACHABLE, e)
 		return
 	}
 
 	done(n.id, distance, nil)
-
-	if distance > 0 {
-
-		n.lock.Lock()
-		defer n.lock.Unlock()
-
-		if n.distance > uint32(distance) {
-			n.nextJmp = nextJmp
-			n.distance = uint32(distance)
-		}
-	}
 }
 
-func (c *clusterImpl[Msg]) getNode(id string) *node {
+// nodeOf 根据 id 获取 node 实例
+// node 作为 cluster 中的单例实现
+func (c *clusterImpl[Msg]) nodeOf(id string) *node {
 	n := c.nodes[id]
 	if n != nil {
 		return n
@@ -341,5 +384,33 @@ func (c *clusterImpl[Msg]) getNode(id string) *node {
 	n = &node{id: id}
 	c.nodes[id] = n
 
+	n.nextJmp.Store(unreachable)
+
 	return n
+}
+
+func (n *nextJmpReference) unreachable() bool {
+	nextJmp := n.Load()
+	return nextJmp == nil || nextJmp.distance == UNREACHABLE
+}
+
+func (n *nextJmpReference) nodeId() string {
+	return n.Load().node.id
+}
+
+func (n *nextJmpReference) distance() uint32 {
+	return n.Load().distance
+}
+
+func (n *nextJmpReference) update(nextJmp *nextJmpNode) {
+	if nextJmp.distance == UNREACHABLE {
+		return
+	}
+
+	snap := n.Load()
+	for (snap == nil || snap.distance > nextJmp.distance) &&
+		!n.CompareAndSwap(snap, nextJmp) {
+
+		snap = n.Load()
+	}
 }
--- distributed/Cluster_test.go
+++ distributed/Cluster_test.go
@@ -26,13 +26,12 @@ type (
 	}
 
 	discoveryReq struct {
-		target   string
-		distance uint32
-		jmp      uint32
+		target string
+		jmp    uint32
 	}
 
 	discoveryResp struct {
-		distance int32
+		distance uint32
 	}
 )
 
@@ -45,23 +44,23 @@ func Test_cluster_Send(t1 *testing.T) {
 	}
 	processors["1"].cluster = NewCluster[any](NewConfig[any](processors["1"])).(*clusterImpl[any])
 	processors["1"].cluster.AddNodes([]string{"1", "2", "3"})
-	processors["1"].cluster.ConnectedNodes([]string{"2"})
+	processors["1"].cluster.SetConnected([]string{"2"})
 
 	processors["2"] = &testProcessor{
 		id:         "2",
 		processors: processors,
 	}
 	processors["2"].cluster = NewCluster[any](NewConfig[any](processors["2"])).(*clusterImpl[any])
-	processors["2"].cluster.AddNodes([]string{"1", "3"})
-	processors["2"].cluster.ConnectedNodes([]string{"1", "3"})
+	processors["2"].cluster.AddNodes([]string{"1", "2", "3"})
+	processors["2"].cluster.SetConnected([]string{"1", "3"})
 
 	processors["3"] = &testProcessor{
 		id:         "3",
 		processors: processors,
 	}
 	processors["3"].cluster = NewCluster[any](NewConfig[any](processors["3"])).(*clusterImpl[any])
-	processors["3"].cluster.AddNodes([]string{"2"})
-	processors["3"].cluster.ConnectedNodes([]string{"2"})
+	processors["3"].cluster.AddNodes([]string{"1", "2", "3"})
+	processors["3"].cluster.SetConnected([]string{"2"})
 
 	send, e := processors["1"].cluster.Send(context.Background(), "3", "hello", 0)
 	fmt.Println(send, e)
@@ -70,7 +69,7 @@ func Test_cluster_Send(t1 *testing.T) {
 	}
 
 	if !send || e != nil {
-		t1.Error()
+		t1.Error(e)
 	}
 }
 
@@ -79,23 +78,25 @@ func (p *testProcessor) send(ctx context.Context, msg any) (any, exceptions.Exce
 	case *msgReq:
 		b := msg.(*msgReq)
 		if b.target == p.id {
-			fmt.Printf("%s received msg: %v\n", p.id, b.msg)
+			fmt.Printf("[%d] %s received msg: %v\n", b.jmp, p.id, b.msg)
 			return &msgResp{true}, nil
 		} else {
+			fmt.Printf("[%d] %s received msg request to %s: %v\n", b.jmp, p.id, b.target, b.msg)
 			send, e := p.cluster.Send(ctx, b.target, b.msg, b.jmp)
 			return &msgResp{send}, e
 		}
 	case *discoveryReq:
 		req := msg.(*discoveryReq)
 		if req.target == p.id {
-			return &discoveryResp{int32(req.distance + 1)}, nil
+			return &discoveryResp{1}, nil
+		}
+
+		distance, e := p.cluster.Find(ctx, req.target, req.jmp)
+
+		if distance == UNREACHABLE {
+			return &discoveryResp{distance}, e
 		} else {
-			distance, e := p.cluster.Find(ctx, req.target, int32(req.distance), req.jmp)
-			if distance < 0 {
-				return &discoveryResp{distance}, e
-			} else {
-				return &discoveryResp{distance + 1}, e
-			}
+			return &discoveryResp{distance + 1}, e
 		}
 	default:
 		return nil, exceptions.NewRuntimeException("unsupported msg", nil)
@@ -124,7 +125,7 @@ func (p *testProcessor) Send(ctx context.Context, nextJmp string, target string,
 	return send.(*msgResp).success, e
 }
 
-func (p *testProcessor) Find(ctx context.Context, nextJmp string, target string, jmp uint32) (int32, exceptions.Exception) {
+func (p *testProcessor) Find(ctx context.Context, nextJmp string, target string, jmp uint32) (uint32, exceptions.Exception) {
 	fmt.Printf("%s find(nextJmp: %s, terget: %s)\n", p.id, nextJmp, target)
 	if target == p.id {
 		return 0, nil
@@ -132,9 +133,8 @@ func (p *testProcessor) Find(ctx context.Context, nextJmp string, target string,
 
 	processor := p.processors[nextJmp]
 	find, e := processor.send(ctx, &discoveryReq{
-		target:   target,
-		distance: 0,
-		jmp:      jmp,
+		target: target,
+		jmp:    jmp,
 	})
 
 	fmt.Printf("%s find(nextJmp: %s, terget: %s) distance %d\n", p.id, nextJmp, target, find.(*discoveryResp).distance)
